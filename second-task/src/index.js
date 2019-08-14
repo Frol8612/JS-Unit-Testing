4b5202bb31661ebe857a2f0fa620b364c2cc4be7
Pavel Fralou <pavel_fralou@epam.com>
2019-08-14 11:22:12 +0300

    refactor

--- second-task/src/index.js
+++ second-task/src/index.js
@@ -5,52 +5,54 @@ const {
 const { assert } = require('chai');
 
 describe('Yndex.Mail', () => {
-  let driver;
+  const driver = new Builder().forBrowser('chrome').build();
+  const loginInput = By.id('passp-field-login');
+  const btnSubmit = By.css('button[type="submit"]');
+  const passwdInput = By.id('passp-field-passwd');
+  const enterInput = By.css('div[name=\'to\']');
+
   before(() => {
-    driver = new Builder().forBrowser('chrome').build();
     driver.get('https://mail.yandex.by/');
+    driver.manage().setTimeouts({ pageLoad: 25000 });
   });
 
   it('should be button', async () => {
-    await driver.wait(until.elementLocated(By.xpath('//a/span[.=\'Войти\']')), 10000);
-    const btn = await driver.findElement(By.xpath('//a[.=\'Войти\']'));
+    const btnEnter = By.xpath('//a[.=\'Войти\']');
+    const btn = await driver.findElement(btnEnter);
     assert.isOk(btn, 'button');
     await btn.click();
   });
 
   it('should don`t be user return error', async () => {
-    await driver.wait(until.elementLocated(By.id('passp-field-login')), 2000);
-    const input = await driver.findElement(By.id('passp-field-login'));
-    const btn = await driver.findElement(By.className('button2_type_submit'));
+    const error = By.xpath('//div[.=\'Такого аккаунта нет\']');
+    await driver.wait(until.elementLocated(loginInput), 2000);
 
-    await input.sendKeys('te786st');
-    await btn.click();
+    await driver.findElement(loginInput).sendKeys('te786st');
+    await driver.findElement(btnSubmit).click();
 
-    await driver.wait(until.elementLocated(By.xpath('//div[.=\'Такого аккаунта нет\']')), 2000);
-    const error = await driver.findElement(By.xpath('//div[.=\'Такого аккаунта нет\']')).getText();
+    await driver.wait(until.elementLocated(error), 2000);
+    const errorTxt = await driver.findElement(error).getText();
 
-    assert.equal(error, 'Такого аккаунта нет');
+    assert.equal(errorTxt, 'Такого аккаунта нет');
   });
 
   it('should return name input login', async () => {
-    const input = await driver.findElement(By.id('passp-field-login'));
-    const btn = await driver.findElement(By.className('button2_type_submit'));
+    const input = await driver.findElement(loginInput);
     const inputTag = await input.getTagName();
     await input.clear();
     await input.sendKeys('test1ng7');
 
     assert.equal(inputTag, 'input');
 
-    await btn.click();
+    await driver.findElement(btnSubmit).click();
   });
 
   it('should return tag input passwd', async () => {
-    await driver.wait(until.elementLocated(By.id('passp-field-passwd')), 2000);
-    const input = await driver.findElement(By.id('passp-field-passwd'));
-    const btn = await driver.findElement(By.className('button2_type_submit'));
-    await input.sendKeys('123456789a');
-    await btn.click();
+    await driver.wait(until.elementLocated(passwdInput), 2000);
+    await driver.findElement(passwdInput).sendKeys('123456789a');
+    await driver.findElement(btnSubmit).click();
     await driver.wait(until.elementLocated(By.className('mail-User-Name')), 5000);
+
     const userName = await driver.findElement(By.className('mail-User-Name')).getText();
     assert.equal(userName, 'test1ng7');
   });
@@ -63,32 +65,27 @@ describe('Yndex.Mail', () => {
   });
 
   it('should return error adress', async () => {
-    await driver.wait(until.elementLocated(By.css('div[name=\'to\']')), 2000);
+    const error = By.className('ns-view-compose-field-to-error');
+    const dropdownMenu = By.css('.b-mail-dropdown__item__content.js-bubble-remove');
+    const btnLabel = By.css('.js-contact-bubble.mail-Bubble-Contact.js-bubble.mail-Bubble');
+    await driver.wait(until.elementLocated(enterInput), 2000);
 
-    const div = await driver.findElement(By.css('div[name=\'to\']'));
-    const btn = await driver.findElement(By.css('.nb-button._nb-large-action-button._init.js-editor-tabfocus-next.js-send.nb-group-start.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only'));
+    await driver.findElement(enterInput).sendKeys('test');
+    await driver.findElement(btnSubmit).click();
 
-    await div.sendKeys('test');
-    await btn.click();
+    await driver.wait(until.elementLocated(error), 2000);
 
-    await driver.wait(until.elementLocated(By.className('ns-view-compose-field-to-error')), 2000);
+    const errorTxt = await driver.findElement(error).getText();
 
-    const error = await driver.findElement(By.className('ns-view-compose-field-to-error')).getText();
+    assert.equal(errorTxt, 'Некорректные адреса: test');
 
-    assert.equal(error, 'Некорректные адреса: test');
-
-    const label = await driver.findElement(By.css('.js-contact-bubble.mail-Bubble-Contact.js-bubble.mail-Bubble'));
-    await label.click();
-    await driver.wait(until.elementLocated(By.css('.b-mail-dropdown__item__content.js-bubble-remove')), 2000);
-    const remove = await driver.findElement(By.css('.b-mail-dropdown__item__content.js-bubble-remove'));
-    await remove.click();
+    await driver.findElement(btnLabel).click();
+    await driver.wait(until.elementLocated(dropdownMenu), 2000);
+    await driver.findElement(dropdownMenu).click();
   });
 
   it('should return name span Себе', async () => {
-    const div = await driver.findElement(By.css('div[name=\'to\']'));
-    const btn = await driver.findElement(By.css('.nb-button._nb-large-action-button._init.js-editor-tabfocus-next.js-send.nb-group-start.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only'));
-
-    assert.isOk(div);
+    const div = await driver.findElement(enterInput);
 
     await div.sendKeys(
       'test1ng7@yandex.ru',
@@ -97,20 +94,24 @@ describe('Yndex.Mail', () => {
       Key.TAB,
       'The letter for you, my frind',
     );
-    await btn.click();
+
+    assert.equal(await div.getText(), 'test1ng7');
+    await driver.findElement(btnSubmit).click();
   });
 
   it('should return text "Письмо отправленно"', async () => {
-    await driver.wait(until.elementLocated(By.xpath('//div[.=\'Письмо отправлено.\']')), 2000);
-    const divText = await driver.findElement(By.xpath('//div[.=\'Письмо отправлено.\']')).getText();
-    const btn = await driver.findElement(By.css('.mail-ComposeButton-Refresh.js-main-action-refresh.ns-action'));
-    assert.equal(divText, 'Письмо отправлено.');
-    await btn.click();
+    const messege = By.xpath('//div[.=\'Письмо отправлено.\']');
+    const btnRefresh = By.css('.mail-ComposeButton-Refresh.js-main-action-refresh.ns-action');
+    await driver.wait(until.elementLocated(messege), 2000);
+
+    assert.equal(await driver.findElement(messege).getText(), 'Письмо отправлено.');
+    await driver.findElement(btnRefresh).click();
+    await driver.wait(until.elementLocated(By.className('mail-MessageSnippet-Content')), 2000);
   });
 
   it('incoming mail', async () => {
-    await driver.wait(until.elementLocated(By.className('mail-MessageSnippet-Content')), 2000);
-    const el = await driver.findElement(By.className('mail-MessageSnippet-Content'));
+    const btnMessage = By.className('mail-MessageSnippet-Content');
+    const el = await driver.findElement(btnMessage);
     el.click();
     assert.isOk(el);
   });
