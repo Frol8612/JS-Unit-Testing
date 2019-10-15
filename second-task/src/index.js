eb528f9f60f019ab299fe608373246ac6e9fa658
Pavel Fralou <pavel_fralou@epam.com>
2019-10-15 17:03:16 +0300

    feat: update test

--- second-task/src/index.js
+++ second-task/src/index.js
@@ -17,37 +17,31 @@ describe('Yndex.Mail', () => {
   });
 
   it('should be button', async () => {
-    const btnEnter = By.xpath('//a[.=\'Войти\']');
+    const btnEnter = By.css('a.HeadBanner-Button-Enter');
     const btn = await driver.findElement(btnEnter);
     assert.isOk(btn, 'button');
     await btn.click();
   });
 
   it('should don`t be user return error', async () => {
-    const error = By.xpath('//div[.=\'Такого аккаунта нет\']');
+    const error = By.css('div.passp-form-field__error');
     await driver.wait(until.elementLocated(loginInput), 2000);
 
     await driver.findElement(loginInput).sendKeys('te786st');
     await driver.findElement(btnSubmit).click();
-
     await driver.wait(until.elementLocated(error), 2000);
-    const errorTxt = await driver.findElement(error).getText();
+    const errorColor = await driver.findElement(error).getCssValue('color');
 
-    assert.equal(errorTxt, 'Такого аккаунта нет');
+    assert.equal(errorColor, 'rgba(223, 75, 65, 1)');
   });
 
-  it('should return name input login', async () => {
+  it('should return name login', async () => {
     const input = await driver.findElement(loginInput);
-    const inputTag = await input.getTagName();
+
     await input.clear();
     await input.sendKeys('test1ng7');
-
-    assert.equal(inputTag, 'input');
-
     await driver.findElement(btnSubmit).click();
-  });
 
-  it('should return tag input passwd', async () => {
     await driver.wait(until.elementLocated(passwdInput), 2000);
     await driver.findElement(passwdInput).sendKeys('123456789a');
     await driver.findElement(btnSubmit).click();
@@ -57,10 +51,11 @@ describe('Yndex.Mail', () => {
     assert.equal(userName, 'test1ng7');
   });
 
-  it('should return name button Написать', async () => {
-    const btn = await driver.findElement(By.xpath('//a[.=\'Написать\']'));
-    const btnText = await btn.getText();
-    assert.equal(btnText, 'Написать');
+  it('should return button "write off"', async () => {
+    const btn = await driver.findElement(By.css('a[href="#compose"]'));
+    const btnName = await btn.getTagName();
+
+    assert.equal(btnName, 'a');
     await btn.click();
   });
 
@@ -75,17 +70,18 @@ describe('Yndex.Mail', () => {
 
     await driver.wait(until.elementLocated(error), 2000);
 
-    const errorTxt = await driver.findElement(error).getText();
+    const errorColor = await driver.findElement(error).getCssValue('background-color');
 
-    assert.equal(errorTxt, 'Некорректные адреса: test');
+    assert.equal(errorColor, 'rgba(230, 21, 21, 0.1)');
 
     await driver.findElement(btnLabel).click();
     await driver.wait(until.elementLocated(dropdownMenu), 2000);
     await driver.findElement(dropdownMenu).click();
   });
 
-  it('should return name span Себе', async () => {
+  it('should return sent to name', async () => {
     const div = await driver.findElement(enterInput);
+    const name = By.className('mail-Bubble-Block_text');
 
     await div.sendKeys(
       'test1ng7@yandex.ru',
@@ -95,12 +91,14 @@ describe('Yndex.Mail', () => {
       'The letter for you, my frind',
     );
 
-    assert.equal(await div.getText(), 'test1ng7');
+    const nameTo = await driver.findElement(name).getText();
+
+    assert.equal(nameTo, 'test1ng7');
     await driver.findElement(btnSubmit).click();
   });
 
   it('should return text "Письмо отправленно"', async () => {
-    const messege = By.xpath('//div[.=\'Письмо отправлено.\']');
+    const messege = By.className('js-title-info');
     const btnRefresh = By.css('.mail-ComposeButton-Refresh.js-main-action-refresh.ns-action');
     await driver.wait(until.elementLocated(messege), 2000);
 
